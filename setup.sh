7e7d4572efc5b1f99b2b029bf5f0566c21f1966f
ChrisLynch96 <chlynch@tcd.ie>
2020-06-01 23:59:29 +0100

    created setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! usr/bin/env bash
+
+# Terminal configuration
+
+# directory configuration
+
+# Git configuration
+
+# Github
+
+# Programming languages
+
+## Java (JDK, JRE, gradle, maven, linting)
+
+## Go
+
+## Node
+
+## React
+
+# vscode and extensions 
+
+# vim config
+
+# brew packages
+
